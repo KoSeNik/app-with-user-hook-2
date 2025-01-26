f8391bbfdab7b51264c6cb8f3f9eb2568fd8d0ca
Serg <serg.komarov@gmail.com>
2025-01-26 17:50:17 +0300

    style: add styles

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
   const [value, { setItem, removeItem }] = useLocalStorage("some-key");
 
   return (
-    <div>
+    <div className="container">
       <p>Значение из LocalStorage: {value}</p>
-      <div>
-        <button onClick={() => setItem("new storage value")}>
+      <div className="buttonContainer">
+        <button className="button" onClick={() => setItem("new storage value")}>
           Задать значение
         </button>
-        <button onClick={() => removeItem()}>Удалить значение</button>
+        <button className="button" onClick={() => removeItem()}>
+          Удалить значение
+        </button>
       </div>
     </div>
   );
